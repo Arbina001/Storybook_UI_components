31be38e8ccd7f712cc89651371a7db297b32205f
arbina.ghanchi <arbina.ghanchi@adrixus.com>
2023-12-29 19:08:47 +0530

    done changes

--- src/stories/Typography/typography.stories.js
+++ src/stories/Typography/typography.stories.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Typography from './Typography';
+import Typography from './typography';
 import { StyledTextContainer } from './Typography.styled';
 
 export default {
