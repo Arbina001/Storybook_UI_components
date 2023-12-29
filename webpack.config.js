682ede9338e1f6aad277c3e7bc5109f1eb594d8f
Arbina001 <153498385+Arbina001@users.noreply.github.com>
2023-12-29 19:29:42 +0530

    Alert (#4)
    * storybook setup
    
    * Alerts are done
    
    * naming convention changes
    
    * changes are done for now
    
    * changes are done for now
    
    * changes are done for now

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
-const path = require('path');
+const path=require('path')
 
-module.exports = {
-  // mode:"devlopnment",
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'final.js',
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+
+module.exports={
+    // mode:"devlopnment",
+    entry:'./index.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'final.js'
     },
-    compress: true,
-    port: 1234,
-  },
-  module: {
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 1234,
+      },
+    module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-        },
-      },
-    ],
+          presets: ['@babel/preset-env','@babel/preset-react']
+        }
+      }
+    ]
   },
-};
+    
+}
