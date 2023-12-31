8ac3bccb328bec24885652889661bc5b944e06e7
Arbina001 <153498385+Arbina001@users.noreply.github.com>
2023-12-31 19:50:50 +0530

    Badgeui (#6)
    * storybook setup
    
    * Badges are done
    
    * changes are done for now
    
    * changes are done for shorthad properties and unwanted code

---
+++ src/stories/theme.js
@@ -0,0 +1,19 @@
+const theme = {
+  primary: '#FF4E54',
+  secondary: '#FF8242',
+  success: '#07CB6E',
+  warning: '#F3B664',
+  lightdarkpink: '#E49393',
+  darkblue: '#060047',
+  lightblue: '#1640D6',
+  darkpink: '#E90064',
+  creampink: '#FF5F9E',
+  lightpurple: '#E26EE5',
+  lightgrey: '#ccc',
+  violet: '#946ee5',
+  darkpurple: '#f3ff',
+  danger: '#FF3D00',
+  white: '#fff',
+
+};
+export default theme;
